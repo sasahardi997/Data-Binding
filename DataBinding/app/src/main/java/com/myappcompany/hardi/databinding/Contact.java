41c193ded1a3c562828aaf10cb0dec799ce72b51
Aleksandar Hardi <sasahardi@gmail.com>
2020-03-11 22:54:42 +0100

    First Commit

---
+++ DataBinding/app/src/main/java/com/myappcompany/hardi/databinding/Contact.java
@@ -0,0 +1,11 @@
+package com.myappcompany.hardi.databinding;
+
+public class Contact {
+
+    public  final String firstNmae, lastName;
+
+    public Contact(String firstNmae, String lastName) {
+        this.firstNmae = firstNmae;
+        this.lastName = lastName;
+    }
+}
